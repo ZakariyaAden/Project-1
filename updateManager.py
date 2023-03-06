49ae412c1ac8b2ebf7a83fde410c45d4d34d44e5
Zak <114368690+ZakariyaAden@users.noreply.github.com>
2023-03-06 10:07:54 -0600

    updating the update scripts haha

--- updateManager.py
+++ updateManager.py
@@ -3,6 +3,8 @@ import os
 #use bash script if you don't have pip 
 #or python installed instead
 
+#I don't think anyone is going to use this  
+
 os.system("javac GradeBook.java")
 os.system("java GradeBook")
 os.system("rm GradeBook.class")
--- updateManger.sh
+++ updateManger.sh
@@ -1,3 +1,5 @@
+#reminder to give exe permission
+
 javac GradeBook.java
 java GradeBook
 rm GradeBook.class
