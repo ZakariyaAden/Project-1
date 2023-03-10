962ed4b4e87ca2bc4243a19746d1498eb830d4ea
Zak <114368690+ZakariyaAden@users.noreply.github.com>
2023-03-10 12:01:17 -0600

    line 29

--- GradeBook.java
+++ GradeBook.java
@@ -1,71 +1,55 @@
-import java.io.*;
-import java.util.*;
+import java.util.HashMap;
 
-public class GradeBook{
-    public static void main(String[] args){
-        Student[] arr = {
-            new Student("Ali"),
-            new Student("Zak"),
-            new Student("Aideed"),
-            new Student("Hamza"),
-        };
-        for (int i = 0; i<arr.length;i++){
-                try {
-                    File file = new File("file" + i + ".txt");
-                    if (file.createNewFile()) {
-                        System.out.println("File created: " + file.getName());
-                    } else {
-                        System.out.println("File already exists.");
-                        file.delete();
-                    }
-                    FileWriter fw = new FileWriter(file);
-                    Random randGradeGenerator = new Random();
-                    String[] possibleGradeArr = {
-                        "A",
-                        "A-",
-                        "B+",
-                        "B",
-                        "B-",
-                        "C+",
-                        "C",
-                        "C-",
-                        "D+",
-                        "D",
-                        "D-",
-                        "F"
-                    };
-                    int randIndex1 = randGradeGenerator.nextInt(11); 
-                    int randIndex2 = randGradeGenerator.nextInt(11);
-                    int randIndex3 = randGradeGenerator.nextInt(11);
-                    arr[i].setLetterGrade(possibleGradeArr[randIndex1],possibleGradeArr[randIndex2],possibleGradeArr[randIndex3]);
-                    Student objForLoop = arr[i];
-                    fw.write(objForLoop.getName());
+public class Student{
+    
+    private String name;
+    private HashMap<String,String> letterGradeHashMap;
+    private int ranking;
 
-                    //have to covert Hashmap to readable string
-                    //have to test if what i did actually did anything
-
-                    //fw.write(objForLoop.getLetterGrade(1));
-                    //fw.write(objForLoop.getLetterGrade(2));
-                    //fw.write(objForLoop.getLetterGrade(3));
-                    fw.write(objForLoop.getLetterGrade());
-                    fw.close();
-                    Scanner fr = new Scanner(file);
-                    int lineCounter = 0;
-                    while(fr.hasNextLine()){    
-                        lineCounter++;
-                    }
-                    if(lineCounter != 2){
-                        //dang
-                    }
-                } catch (IOException e) {
-                    System.out.println("An error occurred.");
-                    e.printStackTrace();
-                }
+    public Student(String name){
+        this.name = name;
+        HashMap<String,String> letterGradeHashMap = new HashMap<>();
+    }
+    public String getName(){
+        return this.name;
+    }
+    public void setLetterGrade(String mathletterGrade, String scienceletterGrade, String englishletterGrade){
+        //if (!mathletterGrade.equals("A") || !mathletterGrade.equals("-A")  || !mathletterGrade.equals("B+") || !mathletterGrade.equals("B") || !mathletterGrade.equals("B-")  || !mathletterGrade.equals("C+") || !mathletterGrade.equals("C") || !mathletterGrade.equals("C-") || !mathletterGrade.equals("D+") || !mathletterGrade.equals("D") || !mathletterGrade.equals("D-")){return;}
+        //if (!scienceletterGrade.equals("A") || !scienceletterGrade.equals("-A")  || !scienceletterGrade.equals("B+") || !scienceletterGrade.equals("B") || !scienceletterGrade.equals("B-")  || !scienceletterGrade.equals("C+") || !scienceletterGrade.equals("C") || !scienceletterGrade.equals("C-") || !scienceletterGrade.equals("D+") || !scienceletterGrade.equals("D") || !scienceletterGrade.equals("D-")){return;}
+        //if (!englishletterGrade.equals("A") || !englishletterGrade.equals("-A")  || !englishletterGrade.equals("B+") || !englishletterGrade.equals("B") || !englishletterGrade.equals("B-")  || !englishletterGrade.equals("C+") || !englishletterGrade.equals("C") || !englishletterGrade.equals("C-") || !englishletterGrade.equals("D+") || !englishletterGrade.equals("D") || !englishletterGrade.equals("D-")){return;}
+        letterGradeHashMap.put("Math",mathletterGrade);
+        letterGradeHashMap.put("Science",scienceletterGrade);
+        letterGradeHashMap.put("English",englishletterGrade);
+    }
+    public String getLetterGrade(int indecator){
+        String grade = null;
+        if(indecator > 3){return null;}
+        switch(indecator){
+            case 1: 
+                if (this.letterGradeHashMap.containsValue("Math") != true){return grade;}
+                grade = "Math: " + this.letterGradeHashMap.get("Math") + "\n";
+                break;
+            case 2: 
+                if (this.letterGradeHashMap.containsValue("Science") != true){return grade;}
+                grade = "Science: " + this.letterGradeHashMap.get("Science") + "\n";
+                break;
+            case 3: 
+                if (this.letterGradeHashMap.containsValue("English") != true){return grade;}
+                grade = "English: " + this.letterGradeHashMap.get("English") + "\n";
+                break;
+            default:
+                break;
+        }
+        return grade;
+    }
+/*
+    public String addAssignment(double score, double possibleScore){
+        
+        double gradePercentage = score/possibleScore;
+        this.gradePercentage = this.gradePercentage + gradePercentage;
+        if (gradePercentage > 100.00){
+            this.gradePercentage = 100.00;
         }
     }
-    public static void rankMethod(Student[] paraArr){
-        for(int i = 0; i < paraArr.length;i++){
-
-        }
-    }
+*/
 }
