c8f23e4aad0c19037b094c1e5fbb4bc22dba311d
Zak <114368690+ZakariyaAden@users.noreply.github.com>
2023-03-10 12:02:56 -0600

    refactor to start production of pug plugin

--- GradeBook.java
+++ GradeBook.java
@@ -1,55 +1,51 @@
-import java.util.HashMap;
+import java.io.*;
+import java.util.*;
 
-public class Student{
-    
-    private String name;
-    private HashMap<String,String> letterGradeHashMap;
-    private int ranking;
+public class GradeBook{
+    public static void main(String[] args){
+        HashMap<Integer,Student> objHashMap = new HashMap<>();
+        Student ali = new Student("Ali");
+        Student zak = new Student("Zak");
+        Student aideed = new Student("Aideed");
+        Student hamza = new Student("Hamza");
+        Random randGradeGenerator = new Random();
+        String[] possibleGradeArr = {
+            "A",
+            "A-",
+            "B+",
+            "B",
+            "B-",
+            "C+",
+            "C",
+            "C-",
+            "D+",
+            "D",
+            "D-",
+            "F"
+        };
+        int randIndex1 = randGradeGenerator.nextInt(possibleGradeArr.length); 
+        int randIndex2 = randGradeGenerator.nextInt(possibleGradeArr.length);
+        int randIndex3 = randGradeGenerator.nextInt(possibleGradeArr.length);
+        ali.setLetterGrade("A","A","A");
+        zak.setLetterGrade("A","A","A");
+        aideed.setLetterGrade("A","A","A");
+        hamza.setLetterGrade("A","A","A");
+        System.out.println(ali.getLetterGrade(1));
+        System.out.println(ali.getLetterGrade(2));
+        System.out.println(ali.getLetterGrade(3));
+        System.out.println(zak.getLetterGrade(1));
+        System.out.println(zak.getLetterGrade(2));
+        System.out.println(zak.getLetterGrade(3));
+        System.out.println(aideed.getLetterGrade(1));
+        System.out.println(aideed.getLetterGrade(2));
+        System.out.println(aideed.getLetterGrade(3));
+        System.out.println(hamza.getLetterGrade(1));
+        System.out.println(hamza.getLetterGrade(2));
+        System.out.println(hamza.getLetterGrade(3));
+    }
+    public static void rankMethod(Student[] paraArr){
+        for(int i = 0; i < paraArr.length;i++){
 
-    public Student(String name){
-        this.name = name;
-        HashMap<String,String> letterGradeHashMap = new HashMap<>();
-    }
-    public String getName(){
-        return this.name;
-    }
-    public void setLetterGrade(String mathletterGrade, String scienceletterGrade, String englishletterGrade){
-        //if (!mathletterGrade.equals("A") || !mathletterGrade.equals("-A")  || !mathletterGrade.equals("B+") || !mathletterGrade.equals("B") || !mathletterGrade.equals("B-")  || !mathletterGrade.equals("C+") || !mathletterGrade.equals("C") || !mathletterGrade.equals("C-") || !mathletterGrade.equals("D+") || !mathletterGrade.equals("D") || !mathletterGrade.equals("D-")){return;}
-        //if (!scienceletterGrade.equals("A") || !scienceletterGrade.equals("-A")  || !scienceletterGrade.equals("B+") || !scienceletterGrade.equals("B") || !scienceletterGrade.equals("B-")  || !scienceletterGrade.equals("C+") || !scienceletterGrade.equals("C") || !scienceletterGrade.equals("C-") || !scienceletterGrade.equals("D+") || !scienceletterGrade.equals("D") || !scienceletterGrade.equals("D-")){return;}
-        //if (!englishletterGrade.equals("A") || !englishletterGrade.equals("-A")  || !englishletterGrade.equals("B+") || !englishletterGrade.equals("B") || !englishletterGrade.equals("B-")  || !englishletterGrade.equals("C+") || !englishletterGrade.equals("C") || !englishletterGrade.equals("C-") || !englishletterGrade.equals("D+") || !englishletterGrade.equals("D") || !englishletterGrade.equals("D-")){return;}
-        letterGradeHashMap.put("Math",mathletterGrade);
-        letterGradeHashMap.put("Science",scienceletterGrade);
-        letterGradeHashMap.put("English",englishletterGrade);
-    }
-    public String getLetterGrade(int indecator){
-        String grade = null;
-        if(indecator > 3){return null;}
-        switch(indecator){
-            case 1: 
-                if (this.letterGradeHashMap.containsValue("Math") != true){return grade;}
-                grade = "Math: " + this.letterGradeHashMap.get("Math") + "\n";
-                break;
-            case 2: 
-                if (this.letterGradeHashMap.containsValue("Science") != true){return grade;}
-                grade = "Science: " + this.letterGradeHashMap.get("Science") + "\n";
-                break;
-            case 3: 
-                if (this.letterGradeHashMap.containsValue("English") != true){return grade;}
-                grade = "English: " + this.letterGradeHashMap.get("English") + "\n";
-                break;
-            default:
-                break;
-        }
-        return grade;
-    }
-/*
-    public String addAssignment(double score, double possibleScore){
-        
-        double gradePercentage = score/possibleScore;
-        this.gradePercentage = this.gradePercentage + gradePercentage;
-        if (gradePercentage > 100.00){
-            this.gradePercentage = 100.00;
         }
     }
-*/
 }
