8ae4129b24a6f8cc943e16dc54dfc6551156a87f
Zak <114368690+ZakariyaAden@users.noreply.github.com>
2023-03-14 12:15:57 -0500

    Update index.js

--- webpage_rework/index.js
+++ webpage_rework/index.js
@@ -1,23 +1,25 @@
 function addButtons(){
-    const div = document.getElementsByClassName("main_div");
+    const div = document.getElementById("main_div");
     let button1 = document.createElement('button');
-    let button2 = document.createElement('button');
-    let buttonAttr1 = document.createAttribute('style');
-    let buttonAttr2 = document.createAttribute('style');
+    let buttonId1 = document.createAttribute('id');
+    let buttonAttr1 = document.createAttribute('class');
 
-    buttonAttr1.value = "backgroud-color: black; height: 50px; width: 50px; border-radius: 50%;";
-    buttonAttr2.value = "backgroud-color: black; height: 50px; width: 50px; border-radius: 50%;";
-
+    let idName1 = 'button';
+    buttonId1.value = idName1;
+    buttonAttr1.value = "buttons";
 
     button1.setAttributeNode(buttonAttr1);
-    button2.setAttributeNode(buttonAttr2);
-    div[0].append(button1);
-    div[0].append(button2);
-
-    div[0].addEventListener('onmouseleave', e => {
-        const buttonArr = document.getElementsByTagName('button');
-        for(let i = 0; i < buttonArr.length;i++){
-            buttonArr[i].remove();
-        }
-    })
+    button1.setAttributeNode(buttonId1);  
+    div.append(button1);
+}
+function deleteButtons(){
+    const div = document.getElementById("main_div");
+    const buttons = document.getElementById('button');
+    div.removeChild(buttons); 
+}
+function showPreview(){
+    const backgroundDiv = document.getElementById('backgroundDiv');
+    const backgroundDivClass = document.createAttribute('class');
+    backgroundDivClass.value = 'backgroundDiv';
+    backgroundDiv.setAttributeNode(backgroundDivClass);
 }
